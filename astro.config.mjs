58149dd0183ee45cf352547c6e063791f3fb41ee
Bryan Vold <bryan_vold@sil.org>
2024-10-19 10:31:28 -0500

    add integrations and i18n sections

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,33 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-
+import tailwind from '@astrojs/tailwind';
+import image from '@astrojs/image';
 import sitemap from '@astrojs/sitemap';
+import compress from 'astro-compress';
 
-// https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+  site: 'https://blog.bryanvold.com',
+  integrations: [
+    mdx(),
+    tailwind(),
+    image({
+      serviceEntryPoint: '@astrojs/image/sharp'
+    }),
+    sitemap(),
+    compress()
+  ],
+  markdown: {
+    shikiConfig: {
+      theme: 'github-dark',
+      wrap: true
+    }
+  },
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en', 'ru', 'de', 'es', 'hi'],
+	routing: {                                                                                          
+		prefixDefaultLocale: true
+	}	
+  }
 });
