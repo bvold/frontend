de2620e16a80499a1001595c7fd5c082f9fe10cd
Bryan Vold <bryan_vold@sil.org>
2024-10-24 15:39:53 -0500

    search works, but need more configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,15 +4,20 @@ import mdx from '@astrojs/mdx';
 import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
 import astroExif from './src/integration/astro-exif';
+import pagefind from 'astro-pagefind';
 
 
 export default defineConfig({
   site: 'https://blog.bryanvold.com',
+  build: {
+    format: 'file',
+  },
   integrations: [
     mdx(),
     tailwind(),
     sitemap(),
     astroExif(),
+    pagefind(),
   ],
   markdown: {
     shikiConfig: {
